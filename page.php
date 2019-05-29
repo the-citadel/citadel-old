795df7e89fbb970db469893f3253aec02f585e8b
Morgan Spencer <mspence4@citadel.edu>
2019-05-29 10:53:35 -0400

    Improved layout of items on page and clarified hierarchy of pages

--- page.php
+++ page.php
@@ -3,18 +3,20 @@
 <?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
 
 <?php while (have_posts()) : the_post(); ?>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 	
 	<?php
 	$args = array(
 		'post_parent' => get_the_ID(), // Current post's ID
+		'post_status' => 'publish',
 	);
 	$children = get_children( $args );
-	
-	if( !empty($children) ) : 
 	?>
+
+	<header class="entry-header<?php if( !empty($children) ) : ?> with-children<?php endif; ?>">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php if( !empty($children) ) : ?>
 	<p class="section-items-pages">
 	<?php
 		$args = array(
@@ -40,8 +42,6 @@
 	</p>
 	<?php endif; ?>
 
-<hr style="margin: 0;"/>
-
 <?php if ( has_nav_menu( 'leftmenu' ) ): ?>
 <aside id="left-sidebar" class="sidebar">
 	<div class="widget_nav_menu">
@@ -49,9 +49,10 @@
 			<a href="<?php echo site_url(); ?>"><i class="fas fa-bars"></i><?php echo bloginfo(); ?></a>
 		</h2>
 		<?php wp_nav_menu( array(
-			'theme_location' => 'primary',
-			'menu_id'        => 'primary',
-			'container' 	 => '',
+			'theme_location' 	=> 'leftmenu',
+			'menu_id'        	=> 'leftmenu',
+			'container' 	 	=> '',
+			'depth'				=> 1,
 		) ); ?>
 	</div>
 </aside>
