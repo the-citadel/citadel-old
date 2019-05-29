ca1962308351004ca1ed3272693c33c2fac207c7
Morgan Spencer <mspence4@citadel.edu>
2019-05-29 10:49:29 -0400

    Updated main menu to show drop downs and descriptions

--- functions.php
+++ functions.php
@@ -215,6 +215,21 @@ function the_breadcrumb()
 
 /*
 ----------
+Add Description Support to Main Menu
+----------
+*/
+
+function prefix_nav_description( $item_output, $item, $depth, $args ) {
+    if ( !empty( $item->description ) ) {
+        $item_output = str_replace( $args->link_after . '</a>', '<span class="menu-item-description">' . $item->description . '</span>' . $args->link_after . '</a>', $item_output );
+    }
+ 
+    return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'prefix_nav_description', 10, 4 );
+
+/*
+----------
 Modify User Permissions
 ----------
 */
