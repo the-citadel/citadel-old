ae3193bda230eced6c88a20ebc8570e5c258aecf
Morgan Spencer <mspence4@citadel.edu>
2019-06-11 10:09:41 -0400

    Modified 404 layout to match pages

--- 404.php
+++ 404.php
@@ -1,27 +1,32 @@
 <?php get_header(); ?>
 
-<?php if (is_active_sidebar('left-sidebar')) { ?>
+
+<header class="entry-header">
+	<h1 class="entry-title">404: Page Not Found</h1>
+</header><!-- .entry-header -->
+
+<?php if ( has_nav_menu( 'leftmenu' ) ): ?>
 <aside id="left-sidebar" class="sidebar">
-	<?php dynamic_sidebar('left-sidebar'); ?>
+	<div class="widget_nav_menu">
+		<h2 class="widgettitle">
+			<a href="<?php echo site_url(); ?>"><i class="fas fa-bars"></i><?php echo bloginfo(); ?></a>
+		</h2>
+		<?php wp_nav_menu( array(
+			'theme_location' 	=> 'leftmenu',
+			'menu_id'        	=> 'leftmenu',
+			'container' 	 	=> '',
+			'depth'				=> 1,
+		) ); ?>
+	</div>
 </aside>
-<?php } ?>
+<?php endif; ?>
 
-<div id="main-content">
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<h1 class="entry-title">404: Page Not Found</h1>
-		</header><!-- .entry-header -->
-		<div class="entry-content">
-			<p>The page you are looking for cannot be found.</p>
-			<p>Please select a menu option, go to <a href="<?php echo get_site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?> Home</a>, or search below.</p>
-			<?php include 'searchform.php'; ?>
-		</div><!-- .entry-content -->
-	</article><!-- #post-## -->
-</div>
+<article id="main-content"<?php if ( !has_nav_menu('leftmenu') ) : ?> class="no-sidebar"<?php endif; ?>>
+	<div class="entry-content">
+		<p>The page you are looking for cannot be found.</p>
+		<p>Please select a menu option, go to <a href="<?php echo get_site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?> Home</a>, or search below.</p>
+		<?php include 'searchform.php'; ?>
+	</div><!-- .entry-content -->
+</article>
 
-<?php if (is_active_sidebar('right-sidebar')) { ?>
-<aside id="right-sidebar" class="sidebar">
-	<?php dynamic_sidebar('right-sidebar'); ?>
-</aside>
-<?php } ?>
 <?php get_footer(); ?>
