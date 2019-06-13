8d935a9006ee0e26614b0382382c12b4f0de7bae
Morgan Spencer <mspence4@citadel.edu>
2019-06-13 18:31:31 -0400

    Updated theme details, modified scroll on search overlay, and removed autocomplete from search

--- searchform.php
+++ searchform.php
@@ -9,12 +9,12 @@
 		<span class="screen-reader-text"><i class="fas fa-times"></i></span>
 	</button>
 	<div class="container">
-		<form role="search" method="get" class="search-form" action="https://www.google.com/search">
+		<form role="search" method="get" class="search-form" action="https://www.google.com/search" autocomplete="off">
 			<div class="field-container">
 				<label for="<?php echo $unique_id; ?>">
 					Search citadel.edu
 				</label>
-				<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="Search The Citadel..." value="<?php echo get_search_query(); ?>" name="q" />
+				<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="Search The Citadel..." value="<?php echo get_search_query(); ?>" name="q" autocomplete="off" />
 				<input type="hidden"  name="sitesearch"
 			value="citadel.edu" />
 				<button type="submit" class="search-submit" aria-label="Search citadel.edu"><span class="screen-reader-text"><i class="fas fa-search"></i></span></button>
