ec8b09187ba58d385a53de2dbd7b996d15e9e8bb
Morgan Spencer <mspence4@citadel.edu>
2019-08-12 14:32:09 -0400

    Overlay search form

--- searchform.php
+++ searchform.php
@@ -16,44 +16,55 @@
 				<button type="submit" class="search-submit" aria-label="Search citadel.edu"><span class="screen-reader-text"><i class="fas fa-search"></i></span></button>
 			</div>
 		</form>
-			<?php
-				$args = array(
-					'site__not_in' 	=> 1,
-					'public'		=> 1,
-				);
+		<?php
 
-				$subsites = get_sites($args);
+			$home_url = get_blog_details( 1 )->siteurl;
+			$faculty = get_blog_id_from_url($home_url);
+			
+			$excluded_sites = array(
+				1,
+				$faculty,
+			);
 
-				// Sort blogs alphabetically.
-				uasort( 
-				    $subsites, 
-				    function( $a, $b ) {
-				        // Compare site blog names alphabetically for sorting purposes.
-				        return strcmp( $a->__get( 'blogname' ), $b->__get( 'blogname' ) );
-				    }
-				);
-		
-				if ( ! empty ( $subsites ) ) {
-					
-					echo '<ul id="site-list">';
 
-					foreach( $subsites as $subsite ) {
-						$subsite_id = get_object_vars( $subsite )["blog_id"];
-						$subsite_name = get_blog_details( $subsite_id )->blogname;
-						$subsite_link = get_blog_details( $subsite_id )->siteurl;
-						if ( get_site_option( 'msregister_blog1_id' ) != $subsite_name && get_site_option( 'msregister_exclude_' . $subsite_name ) != 'yes' ) { 
-								printf(
-									'<li class="list-item visible"><a href="' . $subsite_link . '">' . $subsite_name . '</a></li>',
-									esc_attr( $subsite_name ),
-            						esc_html( $subsite_name )
-            					);
-            			}
-					}
-					
-					echo '</ul>';
+			$args = array(
+				'site__not_in' 	=> array(1),
+				'path__not_in' => 'faculty',
+				'public'		=> 1,
+			);
+
+			$subsites = get_sites($args);
+
+			// Sort blogs alphabetically.
+			uasort( 
+			    $subsites, 
+			    function( $a, $b ) {
+			        // Compare site blog names alphabetically for sorting purposes.
+			        return strcmp( $a->__get( 'blogname' ), $b->__get( 'blogname' ) );
+			    }
+			);
+	
+			if ( ! empty ( $subsites ) ) {
 				
+				echo '<ul id="site-list">';
+
+				foreach( $subsites as $subsite ) {
+					$subsite_id = get_object_vars( $subsite )["blog_id"];
+					$subsite_name = get_blog_details( $subsite_id )->blogname;
+					$subsite_link = get_blog_details( $subsite_id )->siteurl;
+					if ( get_site_option( 'msregister_blog1_id' ) != $subsite_name && get_site_option( 'msregister_exclude_' . $subsite_name ) != 'yes' ) { 
+							printf(
+								'<li class="list-item visible"><a href="' . $subsite_link . '">' . $subsite_name . '</a></li>',
+								esc_attr( $subsite_name ),
+        						esc_html( $subsite_name )
+        					);
+        			}
 				}
-			?>
+				
+				echo '</ul>';
+			
+			}
+		?>
 	</div>
 </section>
 
