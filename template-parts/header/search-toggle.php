6d8b40e004aead15e9213e4f2acc27a9afc1add1
Morgan Spencer <mspence4@citadel.edu>
2019-08-22 18:06:11 -0400

    Initial setup of custom theme framework from scratch

--- template-parts/header/search-toggle.php
+++
@@ -1,3 +0,0 @@
-<button class="search-toggle" type="button" aria-label="Search citadel.edu">
-	<span class="screen-reader-text"><i class="fas fa-search"></i> Search</span>
-</button>
--- template-parts/page/content-page.php
+++
@@ -1,5 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
-		<?php echo the_content(); ?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
