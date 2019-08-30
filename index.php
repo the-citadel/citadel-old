7ed287640c2abd3d5526c490dfba3a41cde3563f
Morgan Spencer <mspence4@citadel.edu>
2019-08-30 09:13:38 -0400

    Added post loop on index to display page content

--- index.php
+++ index.php
@@ -9,6 +9,21 @@
 
 <?php get_header(); ?>
 
+<?php
+if ( have_posts() ) {
 
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content/content' );
+	}
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content', 'none' );
+
+}
+?>
 
 <?php get_footer(); ?>
