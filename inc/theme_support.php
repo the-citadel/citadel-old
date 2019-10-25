4f1a8e2c4d132cd3397d01aa7fe61e74df6b98b9
Morgan Spencer <mspence4@citadel.edu>
2019-10-25 13:04:57 -0400

    Removed main site specific content, wrapped theme support in function, and added border to buttons

--- inc/theme_support.php
+++ inc/theme_support.php
@@ -2,46 +2,64 @@
 
 defined( 'ABSPATH' ) || exit;
 
-// Base Colors
-$citadel_navy = '#1F3A60';
-$citadel_blue = '#3975B7';
-$pt_barracks = '#CEC7BF';
-$shako = '#282E36';
+if ( ! function_exists('citadel_theme_support') ) {
 
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'editor-color-palette', array(
-	array(
-		'name'  => __( 'Citadel Navy', 'citadel' ),
-		'slug'  => 'citadel_navy',
-		'color'	=> $citadel_navy,
-	),
+	// Register Theme Features
+	function citadel_theme_support()  {
 
-	array(
-		'name'  => __( 'Citadel Blue', 'citadel' ),
-		'slug'  => 'citadel_blue',
-		'color' => $citadel_blue,
-	),
+		// Base Colors
+		$citadel_navy = '#1F3A60';
+		$citadel_blue = '#3975B7';
+		$pt_barracks = '#CEC7BF';
+		$shako = '#282E36';
 
-	array(
-		'name'  => __( 'PT Barracks', 'citadel' ),
-		'slug'  => 'pt_barracks',
-		'color' => $pt_barracks,
-	),
+		// Add theme support for Automatic Feed Links
+		add_theme_support( 'automatic-feed-links' );
 
-	array(
-		'name'	=> __( 'White', 'citadel' ),
-		'slug'	=> 'white',
-		'color'	=> '#ffffff',
-	),
+		// Add theme support for Featured Images
+		add_theme_support( 'post-thumbnails' );
 
-	array(
-		'name'	=> __( 'Black', 'citadel' ),
-		'slug'	=> 'black',
-		'color'	=> '#000000',
-	),
+		// Add theme support for HTML5 Semantic Markup
+		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
 
-) );
+		// Gutenberg editor color palette
+		add_theme_support( 'editor-color-palette', array(
+			array(
+				'name'  => __( 'Citadel Navy', 'citadel' ),
+				'slug'  => 'citadel_navy',
+				'color'	=> $citadel_navy,
+			),
 
-add_theme_support( 'disable-custom-colors' );
+			array(
+				'name'  => __( 'Citadel Blue', 'citadel' ),
+				'slug'  => 'citadel_blue',
+				'color' => $citadel_blue,
+			),
+
+			array(
+				'name'  => __( 'PT Barracks', 'citadel' ),
+				'slug'  => 'pt_barracks',
+				'color' => $pt_barracks,
+			),
+
+			array(
+				'name'	=> __( 'White', 'citadel' ),
+				'slug'	=> 'white',
+				'color'	=> '#ffffff',
+			),
+
+			array(
+				'name'	=> __( 'Black', 'citadel' ),
+				'slug'	=> 'black',
+				'color'	=> '#000000',
+			),
+
+		) );
+
+		// Remove custom colors
+		add_theme_support( 'disable-custom-colors' );
+	}
+
+	add_action( 'after_setup_theme', 'citadel_theme_support' );
+
+}
--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 
 defined( 'ABSPATH' ) || exit;
 
-global $blog_id;
-$current_blog_id = $blog_id;
-
-$main_site = 'The Citadel';
-$main_blogname = get_blog_details( 1 )->blogname;
-$blogname = get_bloginfo( 'name' );
-
 ?>
 
 <?php get_header(); ?>
